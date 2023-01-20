da14b8597cd67a328a80673ae01cc32bf3dc74a4
davicab <57443884+davicab@users.noreply.github.com>
2023-01-19 21:30:50 -0300

    avanco

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ const pokeball = document.getElementById("img")
 var selecPoke = document.getElementById("selected")
 
 var selecName = document.getElementById('name')
+var selecType = document.getElementById('type');
 
 var audio = document.getElementById("audio");
+
+var times = 6;
+
+
+let listPokemon = JSON.parse(localStorage.getItem('listaPokemon') || '[]')
 
 pokeball.addEventListener(("click"), () =>{
     animation()
@@ -32,16 +38,40 @@ function animation(){
     }, 300)
 }
 function getPokemon(){
-    var randPok = Math.floor(Math.random() * 915) + 1;
-    selecPoke.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randPok}.png`
-    fetch(`https://pokeapi.co/api/v2/pokemon/${randPok}`)
-        .then(response => response.json())
-        .then(data => {
-            var dataName = data.species.name
-            selecName.innerHTML = dataName.toUpperCase();
-            console.log(data)
-        })
-        .catch(error => {
-            // faça algo com o erro
-        });
+    times--
+    if(times >= 0 && times <= 6){    
+        console.log(times)
+        var randPok = Math.floor(Math.random() * 151) + 1;
+        var dataName;
+        fetch(`https://pokeapi.co/api/v2/pokemon/${randPok}`)
+            .then(response => response.json())
+            .then(data => {
+                selecPoke.src = data.sprites.front_default
+                dataName = data.species.name
+                selecName.innerHTML = dataName.toUpperCase();
+                
+                listPokemon.push(
+                {
+                    "nome" : [dataName],
+                    "data" : [data]
+                }
+                )
+                localStorage.setItem("listaPokemo", JSON.stringify(listPokemon))
+            })
+            .catch(error => {
+                window.alert("pokemon nao disponivel")
+            });
+    }
+}
+function storePoke(){
+
+    let listPokemon = JSON.parse(localStorage.getItem('listaPokemon') || '[]')
+
+    listPokemon.push(
+    {
+        "nome" : [dataName],
+        "id" : [randPok]
+    }
+    )
+    localStorage.setItem("listaPokemo", JSON.stringify(listPokemon))
 }
